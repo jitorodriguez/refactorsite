82788c797a31f6b52ae349f049e884837c23fe81
unknown <jito.rodriguez@outlook.com>
2022-12-19 15:45:46 -0500

    Minor adjustment to css to fit image better on page. Experimented css options for later use.

--- src/components/Mission.tsx
+++ src/components/Mission.tsx
@@ -1,11 +1,19 @@
 import classes from "./Mission.module.css";
 
-const Mission: React.FC<{title: string, message: string, image: string, altImage: string}> = (props) => {
+const Mission: React.FC<{
+  title: string;
+  message: string;
+  image: string;
+  altImage: string;
+}> = (props) => {
   return (
     <div className={classes.mission}>
-      <h2>{props.title}</h2>
-      <p>{props.message}</p>
+      <div>
+        <h2>{props.title}</h2>
+        <p>{props.message}</p>
+      </div>
       <img
+        className={classes.imageFit}
         src={props.image}
         alt={props.altImage}
       ></img>
